4079b136f128d4b3f93abe9d37380836938410b4
Le Zheng <le.zheng@ef.com>
2017-08-25 10:03:58 +0800

    add a functionality to check the balance

--- pages/calculator/index.js
+++ pages/calculator/index.js
@@ -4,8 +4,26 @@ Page({
   data: {
    
   },
-  sliderChange: function () {
-
+  sliderChange: function (event) {
+    const positive = event.detail.value;
+    const personName = event.currentTarget.dataset.personName;
+    let sheet = this.data.sheet; 
+    let personSheet = sheet.get(personName);
+    personSheet.positive = positive;
+    sheet.set(personName, personSheet);
+    const balance = this.getBalance(sheet);
+    this.setData({
+      sheet,
+      balance,
+    });
+  },
+  getBalance: function (sheet) {
+    let balance = 0;
+    [...sheet.values()].forEach((detail) => {
+      balance += detail.number * (detail.positive ? 1 : -1);
+      console.log(balance, detail);
+    });
+    return balance;
   },
   onLoad: function () {
     console.log('onLoad')
@@ -13,8 +31,20 @@ Page({
     wx.getStorage({
       key: 'persons',
       success: function (res) {
+        const persons = res.data;
+        const sheets = [];
+        const sheet = new Map();
+        persons.forEach((person) => 
+          sheet.set(person, {
+            positive: true,
+            number: 1,
+          })
+        );
         that.setData({
-          persons: res.data
+          persons: res.data,
+          sheet,
+          sheets,
+          balance: 0,
         });
       }
     });
