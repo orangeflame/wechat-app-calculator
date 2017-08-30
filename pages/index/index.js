99cd56093858933f0ab8f32fef6c5f55ae1b62c8
Le Zheng <le.zheng@ef.com>
2017-08-30 16:31:53 +0800

    add reset functionality

--- pages/index/index.js
+++ pages/index/index.js
@@ -44,15 +44,10 @@ Page({
       url: '../calculator/index'
     }); 
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this;
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  reset: function() {
+    wx.clearStorage();
+    this.setData({
+      persons: [],
     })
-  }
+  },
 })
